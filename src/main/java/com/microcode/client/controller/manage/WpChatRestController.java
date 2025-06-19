10a1f5da5c49560d84a6dfeb279803bc2721dc48
JAVIER CAMILO GONZALEZ RODRIGUEZ <javgonzalez@grupoasa.local>
2025-06-19 16:16:33 -0500

    Actualización

--- src/main/java/com/microcode/client/controller/manage/WpChatRestController.java
+++ src/main/java/com/microcode/client/controller/manage/WpChatRestController.java
@@ -78,7 +78,7 @@ public class WpChatRestController {
                    """);
             }
 
-            if(wp.getChatAuthenticated() != null && wp.getChatAuthenticated().equals("s")){
+            if(wsp.getChatAuthenticated() != null && wsp.getChatAuthenticated().equals("s")){
                 if(wsp.getActionId() == null || wsp.getActionId().isEmpty()){
                     wp.setWhatsappMessage("No hemos entendido tu solicitud 😓");
                 }else{
