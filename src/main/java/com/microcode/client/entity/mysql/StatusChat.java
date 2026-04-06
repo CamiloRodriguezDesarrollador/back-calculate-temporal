dbc921a78ec2890c11999a8dcf1c2193b78d9955
Javier Camilo Gonzalez Rodriguez <cgonzalez@MAC-LW6LXM4JXX.local>
2026-04-06 12:42:31 -0500

    Actualicaión

--- src/main/java/com/microcode/client/entity/mysql/StatusChat.java
+++ src/main/java/com/microcode/client/entity/mysql/StatusChat.java
@@ -90,7 +90,7 @@ public class StatusChat implements Serializable, Cloneable {
                                 "1. 👷 Trabajador / Extrabajador\n" +
                                 "2. 🏡 Candidato / Cotizar / Proveedor / Validar Certificados \n\n"+
                                 "Al continuar das autorización para el tratamientos de datos personales de acuerdo con la Política de datos. " + urlData + " ." + "\n\n"+
-                                "💼 ¿Quieres ver las vacantes disponibles? Haz clic aquí 👉 https://wa.me/12678002481" +
+                                "💼 ¿Quieres ver las vacantes disponibles? Haz clic aquí 👉 https://wa.me/12678002481" + "\n\n"+
                                 "👉 Para salir del chat en cualquier momento y volver aquí, escribe *EXIT*"
                 )
                 .chatOptions(options.toString())
